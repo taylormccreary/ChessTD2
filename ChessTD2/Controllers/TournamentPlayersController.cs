512bd9ec6bdce5fc7e05455a9bfd17eb1b90b896
Taylor McCreary <taylor@mccreary.cc>
2016-02-15 12:19:46 -0800

    able to add players to a tournament

--- ChessTD2/Controllers/TournamentPlayersController.cs
+++ ChessTD2/Controllers/TournamentPlayersController.cs
@@ -11,22 +11,27 @@ using ChessTD2.Models;
 
 namespace ChessTD2.Controllers
 {
+    public class ParticipantsViewModel
+    {
+        public ICollection<Player> Participants { get; set; }
+        public ICollection<Player> NonParticipants { get; set; }
+        public int TournamentID { get; set; }
+    }
     public class TournamentPlayersController : Controller
     {
         private TDContext db = new TDContext();
 
         public ActionResult Participants(int id)
         {
-            ViewData["tournamentID"] = id;
-            return View(db.Tournaments.Where(t => t.TournamentID == id).First().Players);
-        }
+            var vm = new ParticipantsViewModel();
+            vm.TournamentID = id;
+            vm.Participants = db.Tournaments.Where(t => t.TournamentID == id).First().Players.OrderBy(p=>p.LastName).ToList();
 
-        // GET: TournamentPlayers
-        public ActionResult Index()
-        {
-            return View(db.Players.ToList());
-        }
+            var participantIds = vm.Participants.Select(p => p.PlayerID).ToArray();
+            vm.NonParticipants = db.Players.Where(np => !participantIds.Contains(np.PlayerID)).OrderBy(np=>np.LastName).ToList();
 
+            return View(vm);
+        }
 
 
         // GET: TournamentPlayers/Details/5
@@ -44,93 +49,23 @@ namespace ChessTD2.Controllers
             return View(player);
         }
 
-        // GET: TournamentPlayers/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: TournamentPlayers/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "PlayerID,FirstName,LastName,Rating")] Player player)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Players.Add(player);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(player);
-        }
-
-        // GET: TournamentPlayers/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Player player = db.Players.Find(id);
-            if (player == null)
-            {
-                return HttpNotFound();
-            }
-            return View(player);
-        }
-
-        // POST: TournamentPlayers/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "PlayerID,FirstName,LastName,Rating")] Player player)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(player).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(player);
-        }
-
+        
         public ActionResult Remove(int playerID, int tournamentID)
         {
-            db.Tournaments.Where(t => t.TournamentID == tournamentID).First().Players.Remove(new Player { PlayerID = playerID });
+            var player = db.Players.Find(playerID);
+            db.Tournaments.Include(p=>p.Players).Where(t => t.TournamentID == tournamentID).First().Players.Remove(player);
             db.SaveChanges();
-            return RedirectToAction("Participants", new { id = tournamentID });
+            return RedirectToAction(nameof(TournamentPlayersController.Participants), new { id = tournamentID });
         }
 
-        // GET: TournamentPlayers/Delete/5
-        public ActionResult Delete(int? id)
+        public ActionResult Add(int playerID, int tournamentID)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Player player = db.Players.Find(id);
-            if (player == null)
-            {
-                return HttpNotFound();
-            }
-            return View(player);
-        }
-
-        // POST: TournamentPlayers/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Player player = db.Players.Find(id);
-            db.Players.Remove(player);
+            var player = db.Players.Find(playerID);
+            db.Tournaments.Include(p => p.Players).Where(t => t.TournamentID == tournamentID).First().Players.Add(player);
             db.SaveChanges();
-            return RedirectToAction("Index");
+            return RedirectToAction(nameof(TournamentPlayersController.Participants), new { id = tournamentID });
         }
-
+        
         protected override void Dispose(bool disposing)
         {
             if (disposing)
