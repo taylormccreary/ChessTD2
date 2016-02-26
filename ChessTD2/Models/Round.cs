a1b47ea7eefcbaf62e814e24d598fd4ddc513c7f
Taylor McCreary <taylor@mccreary.cc>
2016-02-26 11:32:52 -0800

    adds Pairing and Round models

---
+++ ChessTD2/Models/Round.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChessTD2.Models
+{
+    public class Round
+    {
+        public int RoundID { get; set; }
+        public Section Section { get; set; }
+        public byte Number { get; set; }
+
+        public virtual ICollection<Pairing> Pairings { get; set; }
+    }
+}
