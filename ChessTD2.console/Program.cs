791c36130851b0034028c3c5824c1541f1dbc041
Taylor McCreary <taylor@mccreary.cc>
2016-05-02 12:57:10 -0700

    creates list of players

--- ChessTD2.console/Program.cs
+++ ChessTD2.console/Program.cs
@@ -11,12 +11,33 @@ namespace ChessTD2.console
 	{
 		static void Main(string[] args)
 		{
-			var section = GetSection();
 			Console.WriteLine("hi!!");
-			Console.WriteLine(section.Rounds.Count());
+
+			var players =
+				from p in GetPlayers()
+				where p.Rating > 100
+				orderby p.Rating descending
+				select p;
+
+			Console.WriteLine(players.Count());
+
 			Console.ReadKey();
 		}
-		
+
+		static List<Player> GetPlayers()
+		{
+			var player1 = new Player { PlayerID = 100, FirstName = "Taylor", LastName = "McCreary", Rating = 2000 };
+			var player2 = new Player { PlayerID = 101, FirstName = "Michael", LastName = "McCreary", Rating = 900 };
+			var player3 = new Player { PlayerID = 102, FirstName = "Debbie", LastName = "McCreary", Rating = 600 };
+			var player4 = new Player { PlayerID = 103, FirstName = "Shannyn", LastName = "McCreary", Rating = 800 };
+			var player5 = new Player { PlayerID = 104, FirstName = "Lily", Rating = 200 };
+			var player6 = new Player { PlayerID = 105, FirstName = "Matt", Rating = 1200 };
+			var player7 = new Player { PlayerID = 106, FirstName = "Sam", Rating = 1300 };
+			var player8 = new Player { PlayerID = 107, FirstName = "John", Rating = 1567 };
+
+			return new List<Player> { player1, player2, player3, player4, player5, player6, player7, player8 };
+		}
+
 		static Section GetSection()
 		{
 			var player1 = new Player { PlayerID = 100, FirstName = "Taylor", LastName = "McCreary", Rating = 2000 };
@@ -24,7 +45,10 @@ namespace ChessTD2.console
 			var player3 = new Player { PlayerID = 102, FirstName = "Debbie", LastName = "McCreary", Rating = 600 };
 			var player4 = new Player { PlayerID = 103, FirstName = "Shannyn", LastName = "McCreary", Rating = 800 };
 			var player5 = new Player { PlayerID = 104, FirstName = "Lily", Rating = 200 };
-			
+			var player6 = new Player { PlayerID = 105, FirstName = "Matt", Rating = 1200 };
+			var player7 = new Player { PlayerID = 106, FirstName = "Sam", Rating = 1300 };
+			var player8 = new Player { PlayerID = 107, FirstName = "John", Rating = 1567 };
+
 			return new Section
 			{
 				Name = "championship",
@@ -73,6 +97,34 @@ namespace ChessTD2.console
 					}
 				}
 			};
+		}
+
+		static void GetScores()
+		{
+			var section = GetSection();
+			var players2 = from p in section.Players
+						   select new
+						   {
+							   Player = p,
+
+							   Score =
+						   (
+						   from ro in section.Rounds
+						   from pr in ro.Pairings
+						   where pr.Black == p || pr.White == p
+						   select
+						   ((pr.Black == p && pr.Result == PairingResult.BlackWins) || (pr.White == p && pr.Result == PairingResult.WhiteWins)) ? 1 :
+						   pr.Result == PairingResult.Draw && (pr.Black == p || pr.White == p) ? 0.5 : 0
+						   ).Sum(),
+
+							   opponents =
+						   (
+						   from ro in section.Rounds
+						   from pr in ro.Pairings
+						   where pr.Black == p || pr.White == p
+						   select pr.Black == p ? pr.White : pr.Black
+						   ).Distinct()
+						   };
 		}
 	}
 }
