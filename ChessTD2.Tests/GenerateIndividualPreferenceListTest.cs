cf751e718e364c5228d5368512f54f32bdcc134c
Taylor McCreary <taylor@mccreary.cc>
2016-07-20 11:04:23 -0700

    sorts score sections

--- ChessTD2.Tests/GenerateIndividualPreferenceListTest.cs
+++ ChessTD2.Tests/GenerateIndividualPreferenceListTest.cs
@@ -8,18 +8,19 @@ namespace ChessTD2.Tests
     [TestFixture]
     public class GenerateIndividualPreferenceListShould
     {
-        Standings standings = new Standings { SectionPlayers = new List<SectionPlayer>() };
+        Standings standings;
         [SetUp]
         public void Setup()
         {
-            standings.SectionPlayers.Add(new SectionPlayer { PlayerID = 001, Rating = 1000, RoundResults = new List<double> {1}, OpponentPlayerIDs = new List<int> { } });
-            standings.SectionPlayers.Add(new SectionPlayer { PlayerID = 002, Rating = 1100, RoundResults = new List<double> {.5}, OpponentPlayerIDs = new List<int> { } });
-            standings.SectionPlayers.Add(new SectionPlayer { PlayerID = 003, Rating = 1200, RoundResults = new List<double> {0}, OpponentPlayerIDs = new List<int> { } });
-            standings.SectionPlayers.Add(new SectionPlayer { PlayerID = 004, Rating = 1300, RoundResults = new List<double> {0}, OpponentPlayerIDs = new List<int> { } });
-            standings.SectionPlayers.Add(new SectionPlayer { PlayerID = 005, Rating = 1400, RoundResults = new List<double> {.5}, OpponentPlayerIDs = new List<int> { } });
-            standings.SectionPlayers.Add(new SectionPlayer { PlayerID = 006, Rating = 1500, RoundResults = new List<double> {0}, OpponentPlayerIDs = new List<int> { } });
-            standings.SectionPlayers.Add(new SectionPlayer { PlayerID = 007, Rating = 1600, RoundResults = new List<double> {1}, OpponentPlayerIDs = new List<int> { } });
-            standings.SectionPlayers.Add(new SectionPlayer { PlayerID = 008, Rating = 1700, RoundResults = new List<double> {1}, OpponentPlayerIDs = new List<int> { } });
+            standings = new Standings { SectionPlayers = new List<SectionPlayer>() };
+            standings.SectionPlayers.Add(new SectionPlayer { PlayerID = 001, Rating = 1000, RoundResults = new List<double> { 1 }, OpponentPlayerIDs = new List<int> { } });
+            standings.SectionPlayers.Add(new SectionPlayer { PlayerID = 002, Rating = 1100, RoundResults = new List<double> { .5 }, OpponentPlayerIDs = new List<int> { } });
+            standings.SectionPlayers.Add(new SectionPlayer { PlayerID = 003, Rating = 1200, RoundResults = new List<double> { 0 }, OpponentPlayerIDs = new List<int> { } });
+            standings.SectionPlayers.Add(new SectionPlayer { PlayerID = 004, Rating = 1300, RoundResults = new List<double> { 0 }, OpponentPlayerIDs = new List<int> { } });
+            standings.SectionPlayers.Add(new SectionPlayer { PlayerID = 005, Rating = 1400, RoundResults = new List<double> { .5 }, OpponentPlayerIDs = new List<int> { } });
+            standings.SectionPlayers.Add(new SectionPlayer { PlayerID = 006, Rating = 1500, RoundResults = new List<double> { 0 }, OpponentPlayerIDs = new List<int> { } });
+            standings.SectionPlayers.Add(new SectionPlayer { PlayerID = 007, Rating = 1600, RoundResults = new List<double> { 1 }, OpponentPlayerIDs = new List<int> { } });
+            standings.SectionPlayers.Add(new SectionPlayer { PlayerID = 008, Rating = 1700, RoundResults = new List<double> { 1 }, OpponentPlayerIDs = new List<int> { } });
         }
 
         [Test]
@@ -56,6 +57,68 @@ namespace ChessTD2.Tests
             Assert.AreEqual(006, list007.ElementAt(4).PlayerID);
             Assert.AreEqual(004, list007.ElementAt(5).PlayerID);
             Assert.AreEqual(003, list007.ElementAt(6).PlayerID);
+        }
+
+        [Test]
+        public void MoveOpponentsToTheBottom()
+        {
+            standings.SectionPlayers.Where(sp => sp.PlayerID == 007).First().OpponentPlayerIDs.Add(005);
+            standings.SectionPlayers.Where(sp => sp.PlayerID == 007).First().OpponentPlayerIDs.Add(001);
+
+            var list007 = Program.GenerateIndividualPreferenceList(standings.SectionPlayers, 007);
+
+            Assert.AreEqual(008, list007.ElementAt(0).PlayerID);
+            Assert.AreEqual(002, list007.ElementAt(1).PlayerID);
+            Assert.AreEqual(006, list007.ElementAt(2).PlayerID);
+            Assert.AreEqual(004, list007.ElementAt(3).PlayerID);
+            Assert.AreEqual(003, list007.ElementAt(4).PlayerID);
+            Assert.AreEqual(001, list007.ElementAt(5).PlayerID);
+            Assert.AreEqual(005, list007.ElementAt(6).PlayerID);
+        }
+
+        [Test]
+        public void SortScoreSectionCorrectly007()
+        {
+            standings.SectionPlayers.Add(new SectionPlayer { PlayerID = 009, Rating = 900, RoundResults = new List<double> { 1 }, OpponentPlayerIDs = new List<int> { } });
+
+            var list007 = Program.GenerateIndividualPreferenceList(standings.SectionPlayers, 007);
+
+            Assert.AreEqual(001, list007.ElementAt(0).PlayerID);
+            Assert.AreEqual(009, list007.ElementAt(1).PlayerID);
+            Assert.AreEqual(008, list007.ElementAt(2).PlayerID);
+            Assert.AreEqual(005, list007.ElementAt(3).PlayerID);
+            Assert.AreEqual(002, list007.ElementAt(4).PlayerID);
+            Assert.AreEqual(006, list007.ElementAt(5).PlayerID);
+            Assert.AreEqual(004, list007.ElementAt(6).PlayerID);
+            Assert.AreEqual(003, list007.ElementAt(7).PlayerID);
+        }
+
+        [Test]
+        public void SortScoreSectionCorrectly002()
+        {
+            standings.SectionPlayers.Add(new SectionPlayer { PlayerID = 009, Rating = 1500, RoundResults = new List<double> { .5 }, OpponentPlayerIDs = new List<int> { } });
+            standings.SectionPlayers.Add(new SectionPlayer { PlayerID = 010, Rating = 1600, RoundResults = new List<double> { .5 }, OpponentPlayerIDs = new List<int> { } });
+            standings.SectionPlayers.Add(new SectionPlayer { PlayerID = 011, Rating = 1700, RoundResults = new List<double> { .5 }, OpponentPlayerIDs = new List<int> { } });
+            standings.SectionPlayers.Add(new SectionPlayer { PlayerID = 012, Rating = 1800, RoundResults = new List<double> { .5 }, OpponentPlayerIDs = new List<int> { } });
+
+
+            var list002 = Program.GenerateIndividualPreferenceList(standings.SectionPlayers, 002);
+
+            Assert.AreEqual(008, list002.ElementAt(0).PlayerID);
+            Assert.AreEqual(007, list002.ElementAt(1).PlayerID);
+            Assert.AreEqual(001, list002.ElementAt(2).PlayerID);
+
+            Assert.AreEqual(012, list002.ElementAt(3).PlayerID);
+            Assert.AreEqual(011, list002.ElementAt(4).PlayerID);
+            Assert.AreEqual(010, list002.ElementAt(5).PlayerID);
+            Assert.AreEqual(009, list002.ElementAt(6).PlayerID);
+
+            Assert.AreEqual(005, list002.ElementAt(7).PlayerID);
+
+
+            Assert.AreEqual(006, list002.ElementAt(8).PlayerID);
+            Assert.AreEqual(004, list002.ElementAt(9).PlayerID);
+            Assert.AreEqual(003, list002.ElementAt(10).PlayerID);
         }
     }
 }
--- ChessTD2.console/Program.cs
+++ ChessTD2.console/Program.cs
@@ -19,7 +19,11 @@ namespace ChessTD2.console
             var standingsByRating = sectionPlayers.OrderByDescending(sp => sp.Rating).ToList();
             for (int i = 0; i < standingsByRating.Count / 2; i++)
             {
-                result.Add(new Pairing { WhitePlayerID = standingsByRating.ElementAt(i).PlayerID, BlackPlayerID = standingsByRating.ElementAt(i + standingsByRating.Count / 2).PlayerID });
+                result.Add(new Pairing
+                {
+                    WhitePlayerID = standingsByRating.ElementAt(i).PlayerID,
+                    BlackPlayerID = standingsByRating.ElementAt(i + standingsByRating.Count / 2).PlayerID
+                });
             }
             return result;
         }
@@ -27,8 +31,54 @@ namespace ChessTD2.console
         // creates individual preference list just by listing the players by rank and removing the given player
         public static List<SectionPlayer> GenerateIndividualPreferenceList(List<SectionPlayer> sectionPlayers, int id)
         {
-            var rankedSectionPlayerList = sectionPlayers.OrderByDescending(sp => sp.RoundResults.Sum()).ThenByDescending(sp => sp.Rating).ToList();
-            rankedSectionPlayerList.Remove(rankedSectionPlayerList.Where(sp => sp.PlayerID == id).First());
+            var currentPlayer = sectionPlayers.Where(sp => sp.PlayerID == id).First();
+            var rankedSectionPlayerList = sectionPlayers
+                .OrderByDescending(sp => sp.RoundResults.Sum())
+                .ThenByDescending(sp => sp.Rating)
+                .ToList();
+
+
+            // sort score section so that opposite half comes first.
+            var scoreSection = rankedSectionPlayerList
+                .Where(sp => sp.RoundResults.Sum() == currentPlayer.RoundResults.Sum())
+                .OrderByDescending(sp => sp.Rating)
+                .ToList();
+
+            var firstHalf = scoreSection.Take(scoreSection.Count() / 2);
+            var secondHalf = scoreSection.Skip(scoreSection.Count() / 2);
+            if (firstHalf.Contains(currentPlayer))
+            {
+                scoreSection = secondHalf.ToList();
+                scoreSection.AddRange(firstHalf);
+                
+                var firstToReplace = rankedSectionPlayerList
+                    .IndexOf
+                    (
+                        rankedSectionPlayerList
+                        .Where(sp => sp.RoundResults.Sum() == currentPlayer.RoundResults.Sum())
+                        .First()
+                    );
+
+                rankedSectionPlayerList.RemoveRange(firstToReplace, scoreSection.Count());
+                rankedSectionPlayerList.InsertRange(firstToReplace, scoreSection);
+            }
+
+            // remove the player whose preference list we're generating
+            rankedSectionPlayerList.Remove
+            (
+                rankedSectionPlayerList.Where(sp => sp.PlayerID == id).First()
+            );
+
+            // move opponnents to the bottom of the list
+            foreach (var player in rankedSectionPlayerList.ToList()) // why the ToList? I'm not sure...see http://stackoverflow.com/questions/604831/collection-was-modified-enumeration-operation-may-not-execute
+            {
+                if (currentPlayer.OpponentPlayerIDs.Contains(player.PlayerID))
+                {
+                    rankedSectionPlayerList.Remove(player);
+                    rankedSectionPlayerList.Add(player);
+                }
+            }
+
             return rankedSectionPlayerList;
         }
     }
