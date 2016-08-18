63cb9bb7fd46954903a3c22ef10559af609357a3
Taylor McCreary <taylor@mccreary.cc>
2016-08-18 14:37:39 -0700

    added TestFixture for odd first round

--- ChessTD2.Tests/CreatePairingsTest.cs
+++ ChessTD2.Tests/CreatePairingsTest.cs
@@ -8,87 +8,237 @@ namespace ChessTD2.Tests
     [TestFixture]
     class CreatePairingsShould
     {
-        Standings standings;
-        Dictionary<int, PreferenceList> prefLists;
-
-        [SetUp]
-        public void Setup()
+        [TestFixture]
+        class ForSimpleFirstRound
         {
-            standings = new Standings { SectionPlayers = new List<SectionPlayer>() };
-            for (int i = 1; i <= 16; i++)
+            Standings standings;
+            List<Pairing> pairings;
+
+            public int HigherStandingPlayerId(int firstID, int secondID)
             {
-                standings.SectionPlayers.Add(new SectionPlayer { PlayerID = i, Rating = i * 100, RoundResults = new List<double> { }, OpponentPlayerIDs = new List<int> { } });
+                var firstPlayer = standings.SectionPlayers.Where(p => p.PlayerID == firstID).First();
+                var firstPlayerIndexInStandings = standings.SectionPlayers.IndexOf(firstPlayer);
+
+                var secondPlayer = standings.SectionPlayers.Where(p => p.PlayerID == secondID).First();
+                var secondPlayerIndexInStandings = standings.SectionPlayers.IndexOf(secondPlayer);
+
+                if (firstPlayerIndexInStandings < secondPlayerIndexInStandings)
+                {
+                    return firstID;
+                }
+                else
+                {
+                    return secondID;
+                }
             }
 
-            standings.SectionPlayers = standings.SectionPlayers
-                .OrderByDescending(p => p.RoundResults.Sum())
-                .ThenByDescending(p => p.Rating)
-                .ThenByDescending(p => p.PlayerID)
-                .ToList();
-
-            prefLists = new Dictionary<int, PreferenceList>();
-            for (int i = 0; i < standings.SectionPlayers.Count(); i++)
+            [SetUp]
+            public void Setup()
             {
-                int id = standings.SectionPlayers.ElementAt(i).PlayerID;
-                prefLists.Add(id, standings.GenerateIndividualPreferenceList(id));
+                standings = new Standings { SectionPlayers = new List<SectionPlayer>() };
+                for (int i = 1; i <= 16; i++)
+                {
+                    standings.SectionPlayers.Add(new SectionPlayer { PlayerID = i, Rating = i * 100, RoundResults = new List<double> { }, OpponentPlayerIDs = new List<int> { } });
+                }
+
+                standings.SectionPlayers = standings.SectionPlayers
+                    .OrderByDescending(p => p.RoundResults.Sum())
+                    .ThenByDescending(p => p.Rating)
+                    .ThenByDescending(p => p.PlayerID)
+                    .ToList();
+                
+                pairings = standings.CreatePairings();
             }
-        }
 
-        [Test]
-        public void ReturnTheCorrectNumberOfPairings()
-        {
-            Assert.AreEqual(standings.SectionPlayers.Count() / 2, standings.CreatePairings().Count());
-        }
-
-        [Test]
-        public void NotOverlapPairings()
-        {
-            var pairings = standings.CreatePairings();
-            var pairedPlayers = new List<int> { };
-            foreach (var pairing in pairings)
+            [Test]
+            public void ReturnTheCorrectNumberOfPairings()
             {
-                pairedPlayers.Add(pairing.WhitePlayerID);
-                pairedPlayers.Add(pairing.BlackPlayerID);
+                Assert.AreEqual(standings.SectionPlayers.Count() / 2, pairings.Count());
             }
-            Assert.AreEqual(pairedPlayers.Count(), pairedPlayers.Distinct().Count());
-        }
 
-        public int HigherStandingPlayerId(int firstID, int secondID)
-        {
-            var firstPlayer = standings.SectionPlayers.Where(p => p.PlayerID == firstID).First();
-            var firstPlayerIndexInStandings = standings.SectionPlayers.IndexOf(firstPlayer);
-
-            var secondPlayer = standings.SectionPlayers.Where(p => p.PlayerID == secondID).First();
-            var secondPlayerIndexInStandings = standings.SectionPlayers.IndexOf(secondPlayer);
-
-            if(firstPlayerIndexInStandings < secondPlayerIndexInStandings)
+            [Test]
+            public void AssignCorrectRoundNumber()
             {
-                return firstID;
+                Assert.AreEqual(1, pairings[0].RoundNumber);
+                Assert.AreEqual(1, pairings[1].RoundNumber);
+                Assert.AreEqual(1, pairings[2].RoundNumber);
+                Assert.AreEqual(1, pairings[3].RoundNumber);
+                Assert.AreEqual(1, pairings[4].RoundNumber);
+                Assert.AreEqual(1, pairings[5].RoundNumber);
+                Assert.AreEqual(1, pairings[6].RoundNumber);
+                Assert.AreEqual(1, pairings[7].RoundNumber);
             }
-            else
+
+            [Test]
+            public void NotOverlapPairings()
             {
-                return secondID;
+                var pairedPlayers = new List<int> { };
+                foreach (var pairing in pairings)
+                {
+                    pairedPlayers.Add(pairing.WhitePlayerID);
+                    pairedPlayers.Add(pairing.BlackPlayerID);
+                }
+                Assert.AreEqual(pairedPlayers.Count(), pairedPlayers.Distinct().Count());
             }
-        }
 
 
-        [Test]
-        public void OrderPairingsByStandings()
-        {
-            var pairings = standings.CreatePairings();
-            for (int i = 0; i < pairings.Count() - 1; i++)
+            [Test]
+            public void OrderPairingsByStandings()
             {
-                var highestInUpperPairing = HigherStandingPlayerId(pairings.ElementAt(i).WhitePlayerID, pairings.ElementAt(i).BlackPlayerID);
-                var highestInLowerPairing = HigherStandingPlayerId(pairings.ElementAt(i+1).WhitePlayerID, pairings.ElementAt(i+1).BlackPlayerID);
-                // check that the highest standing player in this pairing
-                // is higher standing than the highest in the next pairing
-                Assert.AreEqual(highestInUpperPairing, HigherStandingPlayerId(highestInUpperPairing, highestInLowerPairing));
+                for (int i = 0; i < pairings.Count() - 1; i++)
+                {
+                    var highestInUpperPairing = HigherStandingPlayerId(pairings.ElementAt(i).WhitePlayerID, pairings.ElementAt(i).BlackPlayerID);
+                    var highestInLowerPairing = HigherStandingPlayerId(pairings.ElementAt(i + 1).WhitePlayerID, pairings.ElementAt(i + 1).BlackPlayerID);
+                    // check that the highest standing player in this pairing
+                    // is higher standing than the highest in the next pairing
+                    Assert.AreEqual(highestInUpperPairing, HigherStandingPlayerId(highestInUpperPairing, highestInLowerPairing));
+                }
             }
+
+            [Test]
+            public void GenerateCorrectPairings()
+            {
+                Assert.That(pairings[0].WhitePlayerID == 16 || pairings[0].BlackPlayerID == 16);
+                Assert.That(pairings[0].WhitePlayerID == 8 || pairings[0].BlackPlayerID == 8);
+
+                Assert.That(pairings[1].WhitePlayerID == 15 || pairings[1].BlackPlayerID == 15);
+                Assert.That(pairings[1].WhitePlayerID == 7 || pairings[1].BlackPlayerID == 7);
+
+                Assert.That(pairings[2].WhitePlayerID == 14 || pairings[2].BlackPlayerID == 14);
+                Assert.That(pairings[2].WhitePlayerID == 6 || pairings[2].BlackPlayerID == 6);
+
+                Assert.That(pairings[3].WhitePlayerID == 13 || pairings[3].BlackPlayerID == 13);
+                Assert.That(pairings[3].WhitePlayerID == 5 || pairings[3].BlackPlayerID == 5);
+
+                Assert.That(pairings[4].WhitePlayerID == 12 || pairings[4].BlackPlayerID == 12);
+                Assert.That(pairings[4].WhitePlayerID == 4 || pairings[4].BlackPlayerID == 4);
+
+                Assert.That(pairings[5].WhitePlayerID == 11 || pairings[5].BlackPlayerID == 11);
+                Assert.That(pairings[5].WhitePlayerID == 3 || pairings[5].BlackPlayerID == 3);
+
+                Assert.That(pairings[6].WhitePlayerID == 10 || pairings[6].BlackPlayerID == 10);
+                Assert.That(pairings[6].WhitePlayerID == 2 || pairings[6].BlackPlayerID == 2);
+
+                Assert.That(pairings[7].WhitePlayerID == 9 || pairings[7].BlackPlayerID == 9);
+                Assert.That(pairings[7].WhitePlayerID == 1 || pairings[7].BlackPlayerID == 1);
+            }
+
         }
 
         [TestFixture]
-        class GenerateCorrectPairings
+        class ForOddFirstRound
         {
+            Standings standings;
+            List<Pairing> pairings;
+
+            public int HigherStandingPlayerId(int firstID, int secondID)
+            {
+                var firstPlayer = standings.SectionPlayers.Where(p => p.PlayerID == firstID).First();
+                var firstPlayerIndexInStandings = standings.SectionPlayers.IndexOf(firstPlayer);
+
+                var secondPlayer = standings.SectionPlayers.Where(p => p.PlayerID == secondID).First();
+                var secondPlayerIndexInStandings = standings.SectionPlayers.IndexOf(secondPlayer);
+
+                if (firstPlayerIndexInStandings < secondPlayerIndexInStandings)
+                {
+                    return firstID;
+                }
+                else
+                {
+                    return secondID;
+                }
+            }
+
+            [SetUp]
+            public void Setup()
+            {
+                standings = new Standings { SectionPlayers = new List<SectionPlayer>() };
+                for (int i = 1; i <= 17; i++)
+                {
+                    standings.SectionPlayers.Add(new SectionPlayer { PlayerID = i, Rating = i * 100, RoundResults = new List<double> { }, OpponentPlayerIDs = new List<int> { } });
+                }
+
+                standings.SectionPlayers = standings.SectionPlayers
+                    .OrderByDescending(p => p.RoundResults.Sum())
+                    .ThenByDescending(p => p.Rating)
+                    .ThenByDescending(p => p.PlayerID)
+                    .ToList();
+
+                pairings = standings.CreatePairings();
+            }
+
+            [Test]
+            public void ReturnTheCorrectNumberOfPairings()
+            {
+                Assert.AreEqual(standings.SectionPlayers.Count() / 2, pairings.Count());
+            }
+
+            [Test]
+            public void AssignCorrectRoundNumber()
+            {
+                Assert.AreEqual(1, pairings[0].RoundNumber);
+                Assert.AreEqual(1, pairings[1].RoundNumber);
+                Assert.AreEqual(1, pairings[2].RoundNumber);
+                Assert.AreEqual(1, pairings[3].RoundNumber);
+                Assert.AreEqual(1, pairings[4].RoundNumber);
+                Assert.AreEqual(1, pairings[5].RoundNumber);
+                Assert.AreEqual(1, pairings[6].RoundNumber);
+                Assert.AreEqual(1, pairings[7].RoundNumber);
+            }
+
+            [Test]
+            public void NotOverlapPairings()
+            {
+                var pairedPlayers = new List<int> { };
+                foreach (var pairing in pairings)
+                {
+                    pairedPlayers.Add(pairing.WhitePlayerID);
+                    pairedPlayers.Add(pairing.BlackPlayerID);
+                }
+                Assert.AreEqual(pairedPlayers.Count(), pairedPlayers.Distinct().Count());
+            }
+
+
+            [Test]
+            public void OrderPairingsByStandings()
+            {
+                for (int i = 0; i < pairings.Count() - 1; i++)
+                {
+                    var highestInUpperPairing = HigherStandingPlayerId(pairings.ElementAt(i).WhitePlayerID, pairings.ElementAt(i).BlackPlayerID);
+                    var highestInLowerPairing = HigherStandingPlayerId(pairings.ElementAt(i + 1).WhitePlayerID, pairings.ElementAt(i + 1).BlackPlayerID);
+                    // check that the highest standing player in this pairing
+                    // is higher standing than the highest in the next pairing
+                    Assert.AreEqual(highestInUpperPairing, HigherStandingPlayerId(highestInUpperPairing, highestInLowerPairing));
+                }
+            }
+
+            [Test]
+            public void GenerateCorrectPairings()
+            {
+                Assert.That(pairings[0].WhitePlayerID == 16 || pairings[0].BlackPlayerID == 16);
+                Assert.That(pairings[0].WhitePlayerID == 8 || pairings[0].BlackPlayerID == 8);
+
+                Assert.That(pairings[1].WhitePlayerID == 15 || pairings[1].BlackPlayerID == 15);
+                Assert.That(pairings[1].WhitePlayerID == 7 || pairings[1].BlackPlayerID == 7);
+
+                Assert.That(pairings[2].WhitePlayerID == 14 || pairings[2].BlackPlayerID == 14);
+                Assert.That(pairings[2].WhitePlayerID == 6 || pairings[2].BlackPlayerID == 6);
+
+                Assert.That(pairings[3].WhitePlayerID == 13 || pairings[3].BlackPlayerID == 13);
+                Assert.That(pairings[3].WhitePlayerID == 5 || pairings[3].BlackPlayerID == 5);
+
+                Assert.That(pairings[4].WhitePlayerID == 12 || pairings[4].BlackPlayerID == 12);
+                Assert.That(pairings[4].WhitePlayerID == 4 || pairings[4].BlackPlayerID == 4);
+
+                Assert.That(pairings[5].WhitePlayerID == 11 || pairings[5].BlackPlayerID == 11);
+                Assert.That(pairings[5].WhitePlayerID == 3 || pairings[5].BlackPlayerID == 3);
+
+                Assert.That(pairings[6].WhitePlayerID == 10 || pairings[6].BlackPlayerID == 10);
+                Assert.That(pairings[6].WhitePlayerID == 2 || pairings[6].BlackPlayerID == 2);
+
+                Assert.That(pairings[7].WhitePlayerID == 9 || pairings[7].BlackPlayerID == 9);
+                Assert.That(pairings[7].WhitePlayerID == 1 || pairings[7].BlackPlayerID == 1);
+            }
 
         }
     }
