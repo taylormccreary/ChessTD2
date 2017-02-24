658934e3222710802c9a06a3cf6024840e9c508a
Taylor <Taylor@TAYLORSSURFACE>
2017-02-23 20:02:48 -0800

    adds CreatePlayers method in TournamentController

--- ChessTDWebApp/Controllers/TournamentController.cs
+++ ChessTDWebApp/Controllers/TournamentController.cs
@@ -14,5 +14,18 @@ namespace ChessTDWebApp.Controllers
         {
             return View();
         }
+
+        public void CreatePlayers()
+        {
+            List<SectionPlayer> sectionPlayers = new List<SectionPlayer> ();
+            sectionPlayers.Add(new SectionPlayer { PlayerID = 001, Rating = 1000, RoundResults = new List<double> { } });
+            sectionPlayers.Add(new SectionPlayer { PlayerID = 002, Rating = 1100, RoundResults = new List<double> { } });
+            sectionPlayers.Add(new SectionPlayer { PlayerID = 003, Rating = 1200, RoundResults = new List<double> { } });
+            sectionPlayers.Add(new SectionPlayer { PlayerID = 004, Rating = 1300, RoundResults = new List<double> { } });
+            sectionPlayers.Add(new SectionPlayer { PlayerID = 005, Rating = 1400, RoundResults = new List<double> { } });
+            sectionPlayers.Add(new SectionPlayer { PlayerID = 006, Rating = 1500, RoundResults = new List<double> { } });
+            sectionPlayers.Add(new SectionPlayer { PlayerID = 007, Rating = 1600, RoundResults = new List<double> { } });
+            sectionPlayers.Add(new SectionPlayer { PlayerID = 008, Rating = 1700, RoundResults = new List<double> { } });
+        }
     }
 }
