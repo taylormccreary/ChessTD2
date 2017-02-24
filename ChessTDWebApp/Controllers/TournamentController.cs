fd75239e42acadddcf34828ebc29f054bf32f6cb
Taylor <Taylor@TAYLORSSURFACE>
2017-02-24 11:32:20 -0800

    Passes a Player from TournamentController to view

--- ChessTDWebApp/Controllers/TournamentController.cs
+++ ChessTDWebApp/Controllers/TournamentController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using ChessTD2.console;
 
@@ -12,10 +9,13 @@ namespace ChessTDWebApp.Controllers
         // GET: Tournament
         public ActionResult Index()
         {
+            List<SectionPlayer> sPlayers = CreatePlayers();
+            SectionPlayer p = new SectionPlayer { PlayerID = 001, Rating = 1000, RoundResults = new List<double> { } };
+            ViewBag.Player = p;
             return View();
         }
 
-        public void CreatePlayers()
+        public List<SectionPlayer> CreatePlayers()
         {
             List<SectionPlayer> sectionPlayers = new List<SectionPlayer> ();
             sectionPlayers.Add(new SectionPlayer { PlayerID = 001, Rating = 1000, RoundResults = new List<double> { } });
@@ -26,6 +26,8 @@ namespace ChessTDWebApp.Controllers
             sectionPlayers.Add(new SectionPlayer { PlayerID = 006, Rating = 1500, RoundResults = new List<double> { } });
             sectionPlayers.Add(new SectionPlayer { PlayerID = 007, Rating = 1600, RoundResults = new List<double> { } });
             sectionPlayers.Add(new SectionPlayer { PlayerID = 008, Rating = 1700, RoundResults = new List<double> { } });
+
+            return sectionPlayers;
         }
     }
 }
